c89c722537d744ce283274d98aea3d4b4e11dba4
krish160802 <krishkhera16@gmail.com>
2022-12-09 22:49:49 +0530

    Updates

---
+++ precache-manifest.9e1b5496a34ca54f838e5cbdc38329d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c53061017c53f5d7742",
+    "url": "/Cryptofy/static/css/main.f8b321ce.chunk.css"
+  },
+  {
+    "revision": "5c53061017c53f5d7742",
+    "url": "/Cryptofy/static/js/main.5c530610.chunk.js"
+  },
+  {
+    "revision": "f12f5854ce980c3a420c",
+    "url": "/Cryptofy/static/js/1.f12f5854.chunk.js"
+  },
+  {
+    "revision": "0b120443637bf18f8670",
+    "url": "/Cryptofy/static/js/2.0b120443.chunk.js"
+  },
+  {
+    "revision": "4362e5351f896807f725",
+    "url": "/Cryptofy/static/js/runtime~main.4362e535.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Cryptofy/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "efbf382f9556dec81a893c10c8a9dbfc",
+    "url": "/Cryptofy/index.html"
+  }
+];
